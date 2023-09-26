f5b31cf879b290a99a0d60e59f21af5798acc004
Simon Hogg <shoggdev@gmail.com>
2023-09-26 17:04:17 +0100

    Added starting up message to payments service

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,8 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
+  console.log('Payments service starting...');
+
   // Checking that the environment variables we rely on are defined.
   if(!process.env.JWT_KEY) {
     throw new Error('ERROR: Environment variable JWT_KEY not defined.');
