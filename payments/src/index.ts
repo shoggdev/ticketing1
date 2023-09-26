28ce00805828a86940b8cd717ffc79fc4204281e
Simon Hogg <shoggdev@gmail.com>
2023-09-26 17:05:40 +0100

    updated payments starting message:

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
-  console.log('Payments service starting...');
+  console.log('Payments service starting...!');
 
   // Checking that the environment variables we rely on are defined.
   if(!process.env.JWT_KEY) {
